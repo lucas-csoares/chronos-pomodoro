086e6cf9b8d4ce3d9dfabf297a4b34a33dc0ea78
Lucas Soares <lucas-cabral-soares@hotmail.com>
2026-09-22 15:56:29 -0300

    style: Importando e usando arquivos de CSS Modules com escopo de componente

---
+++ src/components/Heading.tsx
@@ -0,0 +1,10 @@
+import styles from './Heading.module.css';
+
+export function Heading() {
+
+    return (
+        <>
+            <h1 className={styles.heading}>Olá, mundo (do Heading)!</h1>
+        </>
+    )
+}
