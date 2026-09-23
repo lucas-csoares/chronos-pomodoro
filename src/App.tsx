17d57be662fcb11da813b87879bc9b3d33f7cd8b
Lucas Soares <lucas-cabral-soares@hotmail.com>
2026-09-23 15:10:35 -0300

    chore: adiciona screenshots das telas

--- src/App.tsx
+++ src/App.tsx
@@ -1,20 +1,22 @@
 import './styles/theme.css';
 import './styles/global.css';
 import { Heading } from './components/Heading';
+import { TimerIcon } from 'lucide-react';
 
 function App() {
     return (
         <>
-            <Heading />
+            <Heading>Olá, Mundo!
+                <button>
+                    <TimerIcon />
+                </button>
+            </Heading>
             <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Eveniet odio delectus dignissimos quae quam a, harum suscipit quas impedit voluptatem placeat esse quo aliquam veritatis dolor deserunt facilis fugiat distinctio!
             Esse amet ipsum harum praesentium ullam eaque? Repellendus repudiandae dolorem tempore aliquam? Impedit voluptatibus debitis, nam sapiente facere qui pariatur veritatis ut, quasi suscipit esse? Nihil soluta nisi minus sint.
             </p>
         </>
-
     )
-    
-    
-    
+       
 }
 
 export {App};
