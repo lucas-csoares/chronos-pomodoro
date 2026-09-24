8f32ae2d672a298e6a2187376d5fd1bc91fc00ae
Lucas Soares <lucas-cabral-soares@hotmail.com>
2026-09-23 22:25:25 -0300

    style: cria componente de logo com ícone e link

--- src/App.tsx
+++ src/App.tsx
@@ -1,19 +1,20 @@
 import './styles/theme.css';
 import './styles/global.css';
-import { Heading } from './components/Heading';
-import { TimerIcon } from 'lucide-react';
+
+import { Heading } from './components/Heading'; 
+import { Container } from './components/Container';
+import { Logo } from './components/Logo';
 
 function App() {
     return (
         <>
-            <Heading>Olá, Mundo!
-                <button>
-                    <TimerIcon />
-                </button>
-            </Heading>
-            <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Eveniet odio delectus dignissimos quae quam a, harum suscipit quas impedit voluptatem placeat esse quo aliquam veritatis dolor deserunt facilis fugiat distinctio!
-            Esse amet ipsum harum praesentium ullam eaque? Repellendus repudiandae dolorem tempore aliquam? Impedit voluptatibus debitis, nam sapiente facere qui pariatur veritatis ut, quasi suscipit esse? Nihil soluta nisi minus sint.
-            </p>
+           <Container>
+                <Logo />
+           </Container>
+
+           <Container>
+                <Heading>MENU</Heading>
+           </Container>
         </>
     )
        
